b628e288d86ccf617757bb9468cd43799d8f98a1
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-06-02 22:40:03 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+let main = document.querySelector("main");
+main.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion"
+
+
+
+// for(let i=0;i<3;i++){
+//     newHeader.textContent = ("YOUR-NAME is the champion").toString();
+
+// }
